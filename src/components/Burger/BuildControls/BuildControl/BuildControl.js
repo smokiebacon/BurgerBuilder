ad17e4b2937fbb84dbdfa08adae83f7dbd5735b7
Abraham Hsu <smokiebacon@gmail.com>
2019-02-10 20:11:53 -0800

    able to add ingredients

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './BuildControl.css'
+
+const BuildControl = (props) => (
+  <div className = {classes.BuildControl}>
+    <div className = {classes.Label}>{props.label}</div>
+    <button className= {classes.More} onClick={props.addIngredientHandler} >More</button>
+    <button className = { classes.Less }>Less</button>
+  </div>
+)
+
+export default BuildControl
